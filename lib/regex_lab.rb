d111dd8d16bce99ab9b5a6e545ebd4bc60594f22
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-20 22:04:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
-  [aeiou].scan
+  word.match(/[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
